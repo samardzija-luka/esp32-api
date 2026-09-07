313458dd6a0eef62bf6392b6a070f7d011200703
samardzija-luka <samardzijaluka6@gmail.com>
2026-09-07 22:08:09 +0200

    Add POST /log endpoint to handle incoming data

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/plain; charset=utf-8"
-    });
 
-    res.end("ESP32 API OK");
+    if (req.method === "POST" && req.url === "/log") {
+
+        let body = "";
+
+        req.on("data", chunk => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+
+            console.log("Primljeni podaci:");
+            console.log(body);
+
+            res.writeHead(200, {
+                "Content-Type": "text/plain; charset=utf-8"
+            });
+
+            res.end("PODACI PRIMLJENI");
+        });
+
+    } else {
+
+        res.writeHead(200, {
+            "Content-Type": "text/plain; charset=utf-8"
+        });
+
+        res.end("ESP32 API OK");
+    }
 });
 
 const PORT = process.env.PORT || 3000;
